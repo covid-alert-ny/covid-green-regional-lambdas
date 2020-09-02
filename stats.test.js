ac5bad45009381b21fa87af2e6648250d2aa4d8d
Mike Whit <mikegwhit@users.noreply.github.com>
2020-09-01 23:53:26 -0600

    Adds testing.

--- stats.test.js
+++ stats.test.js
@@ -1,17 +1,3 @@
-const utilsFile = Object.assign({}, require('./utils'))
-jest.mock('./utils', () => () => (Object.assign({}, utilsFile, {
-    getAssetsBucket: 'foobar'
-})))
-const aws = Object.assign({}, reuqire('aws-sdk'))
-jest.mock('aws-sdk', () => () => (Object.assign({}, aws, {
-    S3: function () {
-        return {
-            putObject: () => ({
-                promise: Promise.resolve
-            })
-        }
-    }
-})))
 const {
     filterDates,
     filterDatesByDateKey,
